af69237c3ef20d71ef1028e88bb6628e5ff481bd
cmoussa1 <moussa1@llnl.gov>
2018-07-16 08:24:59 -0700

    Added docstrings to stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,10 @@
 def compute_avg(scores):
+    """
+    Computes average of a list of numbers.
+
+    :param scores: The list of numbers.
+    :return: An average of the list of numbers.
+    """
 	total = 0
 	for score in scores:
 		total = total + score
@@ -8,7 +14,19 @@ def compute_avg(scores):
 	return avg 
 
 def compute_min(scores):
+    """
+    Returns minimum score.
+
+    :param scores: The list of numbers.
+    :return: The minimum.
+    """
 	return min(scores)
 
 def compute_max(scores):
+    """
+    Returns maximum score.
+
+    :param scores: The list of numbers.
+    :return: The maximum.
+    """
 	return max(scores)

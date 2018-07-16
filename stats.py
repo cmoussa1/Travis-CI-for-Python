bf6dac8bcbfc09db0954ebb520100b2fc79776cb
cmoussa1 <moussa1@llnl.gov>
2018-07-16 08:28:30 -0700

    Changed spaces to tabs in docstrings

--- stats.py
+++ stats.py
@@ -1,10 +1,11 @@
 def compute_avg(scores):
-    """
-    Computes average of a list of numbers.
+	"""
+	Computes average of a list of numbers.
 
-    :param scores: The list of numbers.
-    :return: An average of the list of numbers.
-    """
+	:param scores: The list of numbers.
+	:return: An average of the list of numbers.
+	"""
+
 	total = 0
 	for score in scores:
 		total = total + score
@@ -14,19 +15,21 @@ def compute_avg(scores):
 	return avg 
 
 def compute_min(scores):
-    """
-    Returns minimum score.
+	"""
+	Returns minimum score.
 
-    :param scores: The list of numbers.
-    :return: The minimum.
-    """
+	:param scores: The list of numbers.
+	:return: The minimum.
+	"""
+
 	return min(scores)
 
 def compute_max(scores):
-    """
-    Returns maximum score.
+	"""
+	Returns maximum score.
 
-    :param scores: The list of numbers.
-    :return: The maximum.
-    """
+	:param scores: The list of numbers.
+	:return: The maximum.
+	"""
+
 	return max(scores)
